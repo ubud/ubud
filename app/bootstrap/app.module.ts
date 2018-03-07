d4ebc4be5767ae48cbd0287426ac4ff8224ca64a
Iqbal Maulana <iq.bluejack@gmail.com>
2018-03-07 14:26:54 +0700

    Initial commit

--- app/bootstrap/app.module.ts
+++ app/bootstrap/app.module.ts
@@ -8,14 +8,15 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
