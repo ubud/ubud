7006bb0bdedfd45e09419b971ef20c77a4b4e340
vinverdy <dev.vinverdy@gmail.com>
2018-07-15 22:52:27 +0800

    hotfix: ubud http not working

--- src/ubud/http/src/lib/http-module.ts
+++ src/ubud/http/src/lib/http-module.ts
@@ -1,17 +1,29 @@
 import { InjectionToken, ModuleWithProviders, NgModule, Type } from '@angular/core';
 import { HttpBackend, HttpClient, HttpInterceptor } from '@angular/common/http';
 import { ubudHttpClientFactory } from './factories/http-factory';
+import { BaseUriInterceptor } from './interceptors/base-uri.interceptor';
+
+export interface UbudHttpConfig {
+    baseUri?: string;
+    token: InjectionToken<HttpInterceptor[]>;
+    class: Type<HttpClient>;
+}
 
 @NgModule()
 export class UbudHttpModule {
-    public static forFeature(interceptorToken: InjectionToken<HttpInterceptor[]>, httpClientClass: Type<HttpClient>): ModuleWithProviders {
+    public static forFeature(config: UbudHttpConfig): ModuleWithProviders {
         return {
             ngModule: UbudHttpModule,
             providers: [
                 {
-                    provide: httpClientClass,
+                    provide: config.token,
+                    useValue: new BaseUriInterceptor(config.baseUri || ''),
+                    multi: true,
+                },
+                {
+                    provide: config.class,
                     useFactory: ubudHttpClientFactory,
-                    deps: [HttpBackend, httpClientClass, interceptorToken],
+                    deps: [HttpBackend, config.token],
                 },
             ],
         };
