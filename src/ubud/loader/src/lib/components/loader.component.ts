549137d27ad8707440560a0bddfca3bf104ff520
Iqbal Maulana <iq.bluejack@gmail.com>
2019-02-25 11:57:40 +0700

    feature(loader): add clip rotate type

--- src/ubud/loader/src/lib/components/loader.component.ts
+++ src/ubud/loader/src/lib/components/loader.component.ts
@@ -4,15 +4,20 @@ import { Component, Input } from '@angular/core';
     selector: 'ubud-loader-component',
     template: `
         <span class="la-loader" [ngClass]="['la-' + size, 'la-' + type]">
-            <span></span>
-            <span></span>
-            <span></span>
+            <ng-container *ngIf="'ball-fall' === type">
+                <span></span>
+                <span></span>
+                <span></span>
+            </ng-container>
+            <ng-container *ngIf="'ball-clip-rotate' === type">
+                <span></span>
+            </ng-container>
         </span>
     `,
-    styleUrls: ['../styles/loader.scss'],
+    styleUrls: ['../styles/ball-fall-loader.scss', '../styles/clip-rotate-loader.scss'],
 })
 export class LoaderComponent {
     @Input() public size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
 
-    @Input() public type: 'ball-fall' = 'ball-fall';
+    @Input() public type: 'ball-fall' | 'ball-clip-rotate' = 'ball-fall';
 }
