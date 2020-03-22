22a59741ddf9e30816ba69ce29cbce302a0e82f0
Iqbal Maulana <iq.bluejack@gmail.com>
2020-03-22 15:25:03 +0700

    feature(ngrx): add repository feature for encapsulate query state

---
+++ src/ubud/ngrx/src/lib/repository.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, map, take } from 'rxjs/operators';
+import { Store } from './store';
+
+export abstract class Repository<T> {
+    protected constructor(private store: Store<T>) {}
+
+    public select<R>(project: (state: T) => R): Observable<R> {
+        return this.store.state$.pipe(
+            map(project),
+            distinctUntilChanged(),
+        );
+    }
+
+    public selectOnce<R>(project: (state: T) => R): Observable<R> {
+        return this.select(project).pipe(take(1));
+    }
+}
