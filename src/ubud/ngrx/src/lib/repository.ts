774dcd6a78ba62978941e9d1bb0563b5d49dc613
Iqbal Maulana <iq.bluejack@gmail.com>
2020-03-22 15:37:47 +0700

    fix(ngrx): fix select is not from store

--- src/ubud/ngrx/src/lib/repository.ts
+++ src/ubud/ngrx/src/lib/repository.ts
@@ -6,7 +6,7 @@ export abstract class Repository<T> {
     protected constructor(private store: Store<T>) {}
 
     public select<R>(project: (state: T) => R): Observable<R> {
-        return this.select(project).pipe(
+        return this.store.select(project).pipe(
             map(project),
             distinctUntilChanged(),
         );
